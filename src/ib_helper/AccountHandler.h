3a56b1d0f9fac74f9b54e98f52e88421e2ff0747
John Jones <jmjatlanta@gmail.com>
2023-02-27 12:05:00 -0500

    AccountHandler

--- src/ib_helper/AccountHandler.h
+++ src/ib_helper/AccountHandler.h
@@ -12,13 +12,13 @@ class AccountHandler
     public:
     virtual void OnAccountValueUpdate(const std::string& key, const std::string& value, const std::string& currency,
             const std::string& accountName) = 0;
-    virtual void OnPortfolioUpdate(Contract contract, Decimal position, double marketPrice, double marketValue, 
+    virtual void OnPortfolioUpdate(const Contract& contract, Decimal position, double marketPrice, double marketValue, 
 			double averageCost, double unrealizedPNL, double realizedPNL, const std::string& accountName);
 	virtual void OnUpdateAccountTime(const std::string& timestamp);
 	virtual void OnAccountDownloadEnd(const std::string& accountName);
 	virtual void OnPosition(const std::string& account, Contract contract, Decimal pos, double avgCost);
 	virtual void OnPositionEnd();
-	virtual bool isApproved(Contract contract, Order order);
+	virtual bool isApproved(const Contract& contract, const Order& order);
 	virtual void OnCurrentTime(long currentTime);
 	virtual Position GetPosition(const Contract& contract, const std::string& account);
 	virtual void OnError(const std::exception& e);
