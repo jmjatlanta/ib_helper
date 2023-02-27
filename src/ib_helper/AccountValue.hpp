85e355590a979bb714f80cf3759aa35efbe0587f
John Jones <jmjatlanta@gmail.com>
2023-02-27 16:36:57 -0500

    AccountValue

--- src/ib_helper/AccountValue.hpp
+++ src/ib_helper/AccountValue.hpp
@@ -12,6 +12,8 @@ class AccountValue
     std::string currency;
     std::string account;
 
+    AccountValue() : key(""), value(""), currency(""), account("") {}
+
     AccountValue(const std::string& key, const std::string& value, 
             const std::string& currency, const std::string& account)
         : key(key), value(value), currency(currency), account(account) {}
