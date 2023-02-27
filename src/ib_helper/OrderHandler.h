0e0414a1e4529cde52f115a93b043937f515d53b
John Jones <jmjatlanta@gmail.com>
2023-02-27 12:10:13 -0500

    OrderHandler

--- src/ib_helper/OrderHandler.h
+++ src/ib_helper/OrderHandler.h
@@ -7,7 +7,7 @@
 class OrderHandler 
 {
     public:
-	virtual void OnOpenOrder(int orderId, Contract contract, Order order, OrderState orderState) = 0;
+	virtual void OnOpenOrder(int orderId, const Contract& contract, const Order& order, const OrderState& orderState) = 0;
 	virtual void OnOrderStatus(int orderId, const std::string& status, Decimal filled, Decimal remaining, 
             double avgFillPrice, int permId, int parentId, double lastFillPrice, int clientId, 
             const std::string& whyHeld, double mktCapPrice) = 0;
