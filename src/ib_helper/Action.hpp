4ddee9cfe53a1de3fb2d221b719e640ff2dc49c2
John Jones <jmjatlanta@gmail.com>
2023-03-03 08:39:46 -0500

    Action

--- src/ib_helper/Action.hpp
+++ src/ib_helper/Action.hpp
@@ -3,32 +3,27 @@
 namespace ib_helper
 {
 
-class Action
-{
-    public:
-    enum class ActionType {
-        BUY,
-        SELL,
-        SSHORT,
-        SLONG
-    };
-
-    std::string to_string(ActionType type)
-    {
-        switch(type)
-        {
-            case (ActionType::BUY):
-                return "BUY";
-            case (ActionType::SELL):
-                return "SELL";
-            case (ActionType::SSHORT):
-                return "SSHORT";
-            case (ActionType::SLONG):
-                return "SLONG";
-        }
-    }
-
+enum Action {
+    BUY,
+    SELL,
+    SSHORT,
+    SLONG
 };
+
+std::string to_string(ActionType type)
+{
+    switch(type)
+    {
+        case (ActionType::BUY):
+            return "BUY";
+        case (ActionType::SELL):
+            return "SELL";
+        case (ActionType::SSHORT):
+            return "SSHORT";
+        case (ActionType::SLONG):
+            return "SLONG";
+    }
+}
 
 }
 
