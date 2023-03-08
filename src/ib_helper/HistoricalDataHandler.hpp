dedc3c7fb82a58765e327e1bee8b15156427899f
John Jones <jmjatlanta@gmail.com>
2023-03-08 09:59:21 -0500

    HistoricalDataHandler

--- src/ib_helper/HistoricalDataHandler.hpp
+++ src/ib_helper/HistoricalDataHandler.hpp
@@ -8,8 +8,8 @@ namespace ib_helper
 class HistoricalDataHandler {
     public:
     virtual void OnHistoricalData(int reqId, const Bar& bar) = 0;
-    virtual void OnHistoricalDataEnd(int reqId, const std::string& start, const std::string& end);
-    virtual void OnHistoricalDataUpdate(int reqId, const Bar& bar);
+    virtual void OnHistoricalDataEnd(int reqId, const std::string& start, const std::string& end) = 0;
+    virtual void OnHistoricalDataUpdate(int reqId, const Bar& bar) = 0;
 };
 
 }
