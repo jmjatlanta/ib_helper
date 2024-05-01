406f335f0c8aab04e498e1c6a9852099755943f0
John Jones <jmjatlanta@gmail.com>
2024-05-01 11:37:18 -0500

    Allow for mocking ContractBuilder

--- src/ib_helper/ContractBuilder.hpp
+++ src/ib_helper/ContractBuilder.hpp
@@ -12,11 +12,12 @@ class ContractBuilder
 {
     public:
     ContractBuilder(IBConnector* conn, bool cacheResults = true);
+    ~ContractBuilder() = default;
 
     Contract Build(const std::string& secType, const std::string& ticker);
     Contract Build(SecurityType::Type secType, const std::string& ticker);
 
-    Contract BuildStock(const std::string& ticker);
+    virtual Contract BuildStock(const std::string& ticker);
     
     Contract BuildFuture(const std::string& ticker, time_t now = time(nullptr));
 
@@ -24,7 +25,7 @@ class ContractBuilder
 
     Contract BuildOption(const std::string& ticker);
 
-    ContractDetails GetStockDetails(const std::string& in);
+    virtual ContractDetails GetStockDetails(const std::string& in);
     std::vector<ContractDetails> GetDetails(const Contract& in);
 
     std::vector<ib_helper::SecurityDefinitionOptionParameter> GetOptionParameters(const Contract& opt);
