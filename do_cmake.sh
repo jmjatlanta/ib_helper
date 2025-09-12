ecd2401210ed36227eba7f1d75d86be924c7aa76
John Jones <jmjatlanta@yahoo.com>
2025-09-12 16:48:16 -0400

    more adjustments for new ib_api and protobuf

--- do_cmake.sh
+++ do_cmake.sh
@@ -5,7 +5,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
     -DHH_DATELIB=On \
-    -DIB_CLIENT_DIR=/Users/jmjatlanta/IBJts/source/cppclient/client \
+    -DIB_CLIENT_SOURCE_DIR=/Users/jmjatlanta/IBJts/source \
     ..
 cd ..
 
